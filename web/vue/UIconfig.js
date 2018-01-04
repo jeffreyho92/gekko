0f20ab97075e601def5e0329bc816caaf0629d57
jeffrey <jeffreyho92@gmail.com>
2018-01-04 22:57:57 +0800

    a

--- web/vue/UIconfig.js
+++ web/vue/UIconfig.js
@@ -7,13 +7,14 @@ const CONFIG = {
   headless: false,
   api: {
     host: '127.0.0.1',
-    port: 3000,
+    port: 4000,
     timeout: 120000 // 2 minutes
   },
   ui: {
     ssl: false,
-    host: 'localhost',
-    port: 3000,
+    //host: 'localhost',
+    host: '45.63.18.124',
+    port: 4000,
     path: '/'
   },
   adapter: 'sqlite'
